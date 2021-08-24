781b3da31d0651b60dd72830a15f17eb2b30ce24
ievgend2 <ievgend2@gmail.com>
2021-08-24 15:36:38 -0400

    bug fixed 1

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,6 @@
 import "../../index.css"
+import  Certifications from "/Users/User9/Downloads/portfolio2.0/src/components/Resume/ID Certifications.pdf"
+import  Resume from "/Users/User9/Downloads/portfolio2.0/src/components/Resume/ID Resume.pdf"
 
 function About() {
   return (
@@ -15,10 +17,11 @@ function About() {
         </div>
         <p className="size-large">Hi, I`m Ievgen. I am software developer with need to add here more text below:</p>
         <p className="size-large">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <p className="size-large">You can read more about my biography, experience, skills, education and much more in the PDF attached bellow:</p>
+        <p className="size-large">You can read more about my experience, skills, education and much more in the PDF attached bellow:</p>
         <div className="links size-large">
           <div className="links" data-aos="fade-up">
-            <a href="/portfolio2.0/static/media/ID Resume.b038f8fa.pdf" rel="noopener noreferrer" target="_blank" className="on-hover-40 text-color-red">My resume (pdf 900kb)</a>
+            <a href="/portfolio2.0/static/media/ID Resume.b038f8fa.pdf" rel="noopener noreferrer" target="_blank" className="on-hover-40 text-color-red">My resume (pdf 28kb)</a>
+            <a href= "/portfolio2.0/static/media/ID Certifications.3be71719.pdf" rel="noopener noreferrer" target="_blank" className="on-hover-40 text-color-red">My certifications (pdf 235kb)</a>
           </div>
         </div>
       </div>
