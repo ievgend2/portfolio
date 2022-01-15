8ce0ec0891dc94528e8d816c7c201e84632e6259
ievgend2 <ievgend2@gmail.com>
2022-01-15 18:06:58 -0500

    included project images

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,22 @@
-import { React } from "react";
-import { HashRouter as Router, Route } from 'react-router-dom';
-import "./app.css"
-import "../../index.css"
-import "../../../src/"
+import { React } from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import './app.css'
+import '../../index.css'
+import '../../../src/'
 
-import Navigation from '../Navigation';
-import Skills from '../Skills';
-import Contact from '../Contact';
-import Projects from '../Projects';
+import Navigation from '../Navigation'
+import Skills from '../Skills'
+import Contact from '../Contact'
+import Projects from '../Projects'
 
-import * as ROUTES from '../../constants/routes';
-import Footer from "../Footer";
-import Maintenance from "../Maintenance";
-import MainPage from "../Main";
-import About from "../About";
+import * as ROUTES from '../../constants/routes'
+import Footer from '../Footer'
+import Maintenance from '../Maintenance'
+import MainPage from '../Main'
+import About from '../About'
 
 const App = () => (
-  <Router basename="/portfolio2.0/">
+  <Router basename="/portfolio/">
     <Route exact path={ROUTES.MAINPAGE} component={MainPage} />
     <Route exact path={ROUTES.PROJECTS} component={Projects} />
     <Route exact path={ROUTES.CONTACT} component={Contact} />
@@ -24,6 +24,6 @@ const App = () => (
     <Route exact path={ROUTES.MAINTENANCE} component={Maintenance} />
     <Route exact path={ROUTES.ABOUT} component={About} />
   </Router>
-);
+)
 
-export default App;
+export default App
--- src/components/Maintenance/index.js
+++ src/components/Maintenance/index.js
@@ -1,7 +1,4 @@
-
 function Maintenance() {
-
-
   return (
     <div className="top-bottom-margin website-background-black">
       <h1 className="rotate f-lg">Maintenance</h1>
@@ -9,48 +6,32 @@ function Maintenance() {
       <div className="max-width text-color-white  ">
         <div className="logo">
           <div className="logo-inner">
-            <a href="/portfolio2.0">
+            <a href="/portfolio">
               <div className="label-logo on-hover--40 text-color-white website-background-black"></div>
             </a>
           </div>
         </div>
-        <p className="size-large">Sorry for the inconvenience but we&rsquo;re performing web-site maintenance at the moment. If you need to you can always <a href="#projects" className="text-color-red">go back </a>
-          or <a href="mailto:ievgendotsenko@gmail.com" className="text-color-red">contact us</a> if you have any questions, otherwise we&rsquo;ll be back online shortly!</p>
+        <p className="size-large">
+          Sorry for the inconvenience but we&rsquo;re performing web-site
+          maintenance at the moment. If you need to you can always{' '}
+          <a href="#projects" className="text-color-red">
+            go back{' '}
+          </a>
+          or{' '}
+          <a href="mailto:ievgendotsenko@gmail.com" className="text-color-red">
+            contact us
+          </a>{' '}
+          if you have any questions, otherwise we&rsquo;ll be back online
+          shortly!
+        </p>
         <p className="size-large">The Team</p>
       </div>
     </div>
-
-  );
+  )
 }
 
-export default Maintenance;
+export default Maintenance
 
-{/* <button type="button" onclick="javascript:history.back()">Back</button> */ }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{
+  /* <button type="button" onclick="javascript:history.back()">Back</button> */
+}
